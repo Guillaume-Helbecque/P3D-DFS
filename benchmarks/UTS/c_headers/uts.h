6bc3b3f83f4711d1e4732483c7d84b82d429c9fb
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2023-07-26 10:54:52 +0200

    clean UTS external files

--- benchmarks/UTS/c_headers/uts.h
+++ benchmarks/UTS/c_headers/uts.h
@@ -1,17 +1,12 @@
 /*
- *         ---- The Unbalanced Tree Search (UTS) Benchmark ----
- *
- *  Copyright (c) 2010 See AUTHORS file for copyright holders
- *
- *  This file is part of the unbalanced tree search benchmark.  This
- *  project is licensed under the MIT Open Source license.  See the LICENSE
- *  file for copyright and licensing information.
- *
- *  UTS is a collaborative project between researchers at the University of
- *  Maryland, the University of North Carolina at Chapel Hill, and the Ohio
- *  State University.  See AUTHORS file for more information.
- *
- */
+  This files is derived from the UTS repository of Dinan et al at:
+  https://sourceforge.net/projects/uts-benchmark/
+
+  Reference:
+    S. Olivier, J. Huan, J. Liu, et al. (2007) UTS: An Unbalanced Tree Search
+    Benchmark. 19th International Workshop on Languages and Compilers for Parallel
+    Computing (LCPC). DOI: 10.1007/978-3-540-72521-3_18.
+*/
 
 #ifndef _UTS_H
 #define _UTS_H
@@ -23,31 +18,19 @@ extern "C" {
 #include "rng.h"
 
 /***********************************************************
- *  Tree node descriptor and statistics                    *
+ *  Tree node descriptor                                   *
  ***********************************************************/
 
-#define MAXNUMCHILDREN    100  // cap on children (BIN root is exempt)
+#define MAXNUMCHILDREN 100  // cap on children (BIN root is exempt)
 
-struct node_t {
-  int dist;          // distribution governing number of children
-  int height;        // depth of this node in the tree
-  int numChildren;   // number of children, -1 => not yet determined
+typedef struct
+{
+  int dist;             // distribution governing number of children
+  int height;           // depth of this node in the tree
+  int numChildren;      // number of children, -1 => not yet determined
 
-  /* for statistics (if configured via UTS_STAT) */
-#ifdef UTS_STAT
-  struct node_t *pp;          // parent pointer
-  int sizeChildren;           // sum of children sizes
-  int maxSizeChildren;        // max of children sizes
-  int ind;
-  int size[MAXNUMCHILDREN];   // children sizes
-  double unb[MAXNUMCHILDREN]; // imbalance of each child 0 <= unb_i <= 1
-#endif
-
-  /* for RNG state associated with this node */
-  struct state_t state;
-};
-
-typedef struct node_t Node_UTS;
+  struct state_t state; // RNG state associated with this node
+} Node_UTS;
 
 /* Tree type
  *   Trees are generated using a Galton-Watson process, in
@@ -62,35 +45,30 @@ typedef struct node_t Node_UTS;
 enum uts_trees_e    { BIN = 0, GEO, HYBRID, BALANCED };
 enum uts_geoshape_e { LINEAR = 0, EXPDEC, CYCLIC, FIXED };
 
-typedef enum uts_trees_e    tree_t;
+typedef enum uts_trees_e tree_t;
 typedef enum uts_geoshape_e geoshape_t;
 
 /* Strings for the above enums */
 extern char* uts_trees_str[];
 extern char* uts_geoshapes_str[];
 
-/* For stats generation */
-typedef unsigned long long counter_t;
-
 /* Utility Functions */
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
-#define MIN(a,b) (((a) < (b)) ? (a) : (b)) // not used
-
-// void   uts_showStats(int nPes, int chunkSize, double walltime, counter_t nNodes, counter_t nLeaves, counter_t maxDepth);
-double uts_wctime();
 
 double rng_toProb(int n);
 
 /* Common tree routines */
-void  uts_initRoot(Node_UTS *root, tree_t treeType, int rootId);
-int   uts_numChildren(Node_UTS *parent, tree_t treeType, int nonLeafBF, double nonLeafProb,
-  double b_0, geoshape_t shape_fn, int gen_mx, double shiftDepth);
-int   uts_numChildren_bin(Node_UTS *parent, int nonLeafBF, double nonLeafProb);
-int   uts_numChildren_geo(Node_UTS *parent, double b_0, geoshape_t shape_fn, int gen_mx);
-int   uts_childType(Node_UTS *parent, tree_t treeType, double shiftDepth, int gen_mx);
+void uts_initRoot(Node_UTS* root, tree_t treeType, int rootId);
 
-void c_decompose(Node_UTS *parent, Node_UTS children[], tree_t treeType, int numChildren,
-  int gen_mx, double shiftDepth, int computeGranularity, int *treeSize, int *maxDepth);
+int uts_numChildren_bin(Node_UTS* parent, int nonLeafBF, double nonLeafProb);
+int uts_numChildren_geo(Node_UTS* parent, double b_0, geoshape_t shape_fn, int gen_mx);
+int uts_numChildren(Node_UTS* parent, tree_t treeType, int nonLeafBF, double nonLeafProb,
+  double b_0, geoshape_t shape_fn, int gen_mx, double shiftDepth);
+
+int uts_childType(Node_UTS* parent, tree_t treeType, double shiftDepth, int gen_mx);
+
+void c_decompose(Node_UTS* parent, Node_UTS children[], tree_t treeType, int numChildren,
+  int gen_mx, double shiftDepth, int computeGranularity, int* treeSize, int* maxDepth);
 
 #ifdef __cplusplus
 }
--- benchmarks/UTS/c_sources/uts.c
+++ benchmarks/UTS/c_sources/uts.c
@@ -1,33 +1,22 @@
 /*
- *         ---- The Unbalanced Tree Search (UTS) Benchmark ----
- *
- *  Copyright (c) 2010 See AUTHORS file for copyright holders
- *
- *  This file is part of the unbalanced tree search benchmark.  This
- *  project is licensed under the MIT Open Source license.  See the LICENSE
- *  file for copyright and licensing information.
- *
- *  UTS is a collaborative project between researchers at the University of
- *  Maryland, the University of North Carolina at Chapel Hill, and the Ohio
- *  State University.  See AUTHORS file for more information.
- *
- */
+  This files is derived from the UTS repository of Dinan et al at:
+  https://sourceforge.net/projects/uts-benchmark/
+
+  Reference:
+    S. Olivier, J. Huan, J. Liu, et al. (2007) UTS: An Unbalanced Tree Search
+    Benchmark. 19th International Workshop on Languages and Compilers for Parallel
+    Computing (LCPC). DOI: 10.1007/978-3-540-72521-3_18.
+*/
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
-#include <sys/time.h>
-#ifdef sgi
-#include <time.h>
-#else
-#include <sys/time.h>
-#endif
 
 #include "../c_headers/uts.h"
 
-char * uts_trees_str[]     = { "Binomial", "Geometric", "Hybrid", "Balanced" };
-char * uts_geoshapes_str[] = { "Linear decrease", "Exponential decrease", "Cyclic",
+char* uts_trees_str[]     = { "Binomial", "Geometric", "Hybrid", "Balanced" };
+char* uts_geoshapes_str[] = { "Linear decrease", "Exponential decrease", "Cyclic",
                               "Fixed branching factor" };
 
 /***********************************************************
@@ -36,33 +25,12 @@ char * uts_geoshapes_str[] = { "Linear decrease", "Exponential decrease", "Cycli
  *                                                         *
  ***********************************************************/
 
-/*
- * wall clock time
- *   for detailed accounting of work, this needs
- *   high resolution
- */
-#ifdef sgi
-double uts_wctime() {
-  timespec_t tv;
-  double time;
-  clock_gettime(CLOCK_SGI_CYCLE,&tv);
-  time = ((double) tv.tv_sec) + ((double)tv.tv_nsec / 1e9);
-  return time;
-}
-#else
-double uts_wctime() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return (tv.tv_sec + 1E-6 * tv.tv_usec);
-}
-#endif
-
 // Interpret 32 bit positive integer as value on [0,1)
 double rng_toProb(int n)
 {
-  if (n < 0) {
+  if (n < 0)
     printf("*** toProb: rand n = %d out of range\n",n);
-  }
+
   return ((n<0)? 0.0 : ((double) n)/2147483648.0);
 }
 
@@ -74,7 +42,7 @@ void uts_initRoot(Node_UTS* root, tree_t treeType, int rootId)
   rng_init(root->state.state, rootId);
 }
 
-int uts_numChildren_bin(Node_UTS * parent, int nonLeafBF, double nonLeafProb)
+int uts_numChildren_bin(Node_UTS* parent, int nonLeafBF, double nonLeafProb)
 {
   // distribution is identical everywhere below root
   int    v = rng_rand(parent->state.state);
@@ -87,62 +55,59 @@ int uts_numChildren_geo(Node_UTS* parent, double b_0, geoshape_t shape_fn, int g
 {
   double b_i = b_0;
   int depth = parent->height;
-  int numChildren, h;
-  double p, u;
 
   // use shape function to compute target b_i
-  if (depth > 0){
+  if (depth > 0) {
     switch (shape_fn) {
 
       // expected size polynomial in depth
-    case EXPDEC:
-      b_i = b_0 * pow((double) depth, -log(b_0)/log((double) gen_mx));
-      break;
+      case EXPDEC:
+        b_i = b_0 * pow((double) depth, -log(b_0)/log((double) gen_mx));
+        break;
 
       // cyclic tree size
-    case CYCLIC:
-      if (depth > 5 * gen_mx){
-        b_i = 0.0;
+      case CYCLIC:
+        if (depth > 5 * gen_mx) {
+          b_i = 0.0;
+          break;
+        }
+        b_i = pow(b_0, sin(2.0*3.141592653589793*(double) depth / (double) gen_mx));
         break;
-      }
-      b_i = pow(b_0, sin(2.0*3.141592653589793*(double) depth / (double) gen_mx));
-      break;
 
       // identical distribution at all nodes up to max depth
-    case FIXED:
-      b_i = (depth < gen_mx)? b_0 : 0;
-      break;
+      case FIXED:
+        b_i = (depth < gen_mx)? b_0 : 0;
+        break;
 
       // linear decrease in b_i
-    case LINEAR:
-    default:
-      b_i =  b_0 * (1.0 - (double) depth / (double) gen_mx);
-      break;
+      case LINEAR:
+
+      default:
+        b_i =  b_0 * (1.0 - (double) depth / (double) gen_mx);
+        break;
     }
   }
 
   // given target b_i, find prob p so expected value of
   // geometric distribution is b_i.
-  p = 1.0 / (1.0 + b_i);
+  double p = 1.0 / (1.0 + b_i);
 
   // get uniform random number on [0,1)
-  h = rng_rand(parent->state.state);
-  u = rng_toProb(h);
+  int h = rng_rand(parent->state.state);
+  double u = rng_toProb(h);
 
   // max number of children at this cumulative probability
   // (from inverse geometric cumulative density function)
-  numChildren = (int) floor(log(1 - u) / log(1 - p));
+  int numChildren = (int) floor(log(1 - u) / log(1 - p));
 
   return numChildren;
 }
 
-
-int uts_numChildren(Node_UTS *parent, tree_t treeType, int nonLeafBF, double nonLeafProb,
+int uts_numChildren(Node_UTS* parent, tree_t treeType, int nonLeafBF, double nonLeafProb,
   double b_0, geoshape_t shape_fn, int gen_mx, double shiftDepth)
 {
-  int numChildren = 0;
+  int numChildren;
 
-  /* Determine the number of children */
   switch (treeType) {
     case BIN:
       if (parent->height == 0)
@@ -211,56 +176,23 @@ int uts_childType(Node_UTS* parent, tree_t treeType, double shiftDepth, int gen_
   }
 }
 
-// construct string with all parameter settings
-// int uts_paramsToStr(char *strBuf, int ind) {
-//
-//   // random number generator
-//   ind += sprintf(strBuf+ind, "Random number generator: ");
-//   ind  = rng_showtype(strBuf, ind);
-//
-//   return ind;
-// }
-
-// void uts_showStats(int nPes, int chunkSize, double walltime, counter_t nNodes, counter_t nLeaves, counter_t maxDepth) {
-//   // summarize execution info for machine consumption
-//   if (verbose == 0) {
-//     printf("%4d %7.3f %9llu %7.0llu %7.0llu %d %d %.2f %d %d %1d %f %3d\n",
-//         nPes, walltime, nNodes, (long long)(nNodes/walltime), (long long)((nNodes/walltime)/nPes), chunkSize,
-//         treeType, b_0, rootId, gen_mx, shape_fn, nonLeafProb, nonLeafBF);
-//   }
-//
-//   // summarize execution info for human consumption
-//   else {
-//     printf("Tree size = %llu, tree depth = %llu, num leaves = %llu (%.2f%%)\n", nNodes, maxDepth, nLeaves, nLeaves/(float)nNodes*100.0);
-//     printf("Wallclock time = %.3f sec, performance = %.0f nodes/sec (%.0f nodes/sec per PE)\n\n",
-//         walltime, (nNodes / walltime), (nNodes / walltime / nPes));
-//   }
-// }
-
-void c_decompose(Node_UTS *parent, Node_UTS children[], tree_t treeType, int numChildren,
-  int gen_mx, double shiftDepth, int computeGranularity, int *treeSize, int *maxDepth)
+void c_decompose(Node_UTS* parent, Node_UTS children[], tree_t treeType, int numChildren,
+  int gen_mx, double shiftDepth, int computeGranularity, int* treeSize, int* maxDepth)
 {
-  int parentHeight = parent->height;
-  int childType;
+  int childrenHeight = parent->height + 1;
+  int childType = childType = uts_childType(parent, treeType, shiftDepth, gen_mx);
 
-  childType   = uts_childType(parent, treeType, shiftDepth, gen_mx);
+  for (int i = 0; i < numChildren; i++) {
+    children[i].dist = childType;
+    children[i].height = childrenHeight;
+    children[i].numChildren = -1;
 
-  // construct children and push onto stack
-  int i, j;
-  Node_UTS child;
-
-  child.dist = childType;
-  child.height = parentHeight + 1;
-
-  for (i = 0; i < numChildren; i++) {
-    for (j = 0; j < computeGranularity; j++) {
-      // computeGranularity controls number of rng_spawn calls per node
-      rng_spawn(parent->state.state, child.state.state, i);
+    // 'computeGranularity' controls the number of 'rng_spawn' calls per node evaluation.
+    for (int j = 0; j < computeGranularity; j++) {
+      rng_spawn(parent->state.state, children[i].state.state, i);
     }
-     children[i] = child;
   }
-  *treeSize += numChildren;
-  *maxDepth = MAX(*maxDepth, parentHeight + 1);
 
-  return;
+  *treeSize += numChildren;
+  *maxDepth = MAX(*maxDepth, childrenHeight);
 }
